a072f1c35ca46b988f8d07b67996c56de7a161cf
jiharal <jihar.akakom14@gmail.com>
2020-05-11 17:00:17 +0700

    remove testify package

--- libminio_test.go
+++ libminio_test.go
@@ -9,8 +9,6 @@ import (
 	"net/http"
 	"os"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 func TestLibMinio(t *testing.T) {
@@ -26,16 +24,22 @@ func TestLibMinio(t *testing.T) {
 	defer file.Close()
 
 	fileImg, _, err := image.Decode(file)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	fileName := "content/square/marketplace/3bed03b8-76db-4526-8fb4-18f581dd958b.jpg"
 	var newBuf bytes.Buffer
 
 	err = jpeg.Encode(&newBuf, fileImg, nil)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	contentType := http.DetectContentType(newBuf.Bytes())
 	t.Log(contentType)
 	t.Log(fileName)
 	url, err := client.Upload(fileName, newBuf.Bytes(), int64(newBuf.Len()), http.DetectContentType(newBuf.Bytes()))
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	log.Println(url)
 }
